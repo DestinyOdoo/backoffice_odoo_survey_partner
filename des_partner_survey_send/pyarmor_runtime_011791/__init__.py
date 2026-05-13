d34fd395e806821871dad7b09a634a144c9b3e0e
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-05-13 12:55:56 -0500

    [ADD] Add compilate with page

--- des_partner_survey_send/pyarmor_runtime_011791/__init__.py
+++ des_partner_survey_send/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-11T14:38:24.950755
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-13T12:46:34.382576
 from odoo.addons.des_partner_survey_send.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
