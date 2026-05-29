e9ea5565379bda863a70655e88fe6473663cc64f
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-05-29 15:00:40 -0500

    [DEL] Delete manuals before version

--- des_partner_survey_send/pyarmor_runtime_011791/__init__.py
+++ des_partner_survey_send/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:21:50.651024
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:58:49.010043
 from odoo.addons.des_partner_survey_send.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
--- des_survey_reminder/pyarmor_runtime_011791/__init__.py
+++ des_survey_reminder/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:21:50.651024
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:58:49.010043
 from odoo.addons.des_survey_reminder.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
