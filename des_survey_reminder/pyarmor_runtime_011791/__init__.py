ff5b1ac06ee4fa8706d80290496cfe91cce7017c
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-07-12 20:01:28 -0500

    [ADD] Add ofuscate files and manuals 20260712

--- des_survey_reminder/pyarmor_runtime_011791/__init__.py
+++ des_survey_reminder/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-06-04T16:55:18.217099
+# Pyarmor 9.2.4 (pro), 011791, 2026-07-12T10:02:04.860518
 from odoo.addons.des_survey_reminder.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
